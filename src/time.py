d3c1001730d7e3e9e5cf0dd22913a68dfce469c2
Lacey Perry <lcurtisp@gmail.com>
2016-10-23 07:35:56 +0000

    This is a working version, has not been tested exhaustively. Will need to check it again after Nov 6th to make sure it works when DST changes.

--- src/time.py
+++ src/time.py
@@ -1,95 +1,174 @@
 """
-@author <insert name>
-<insert desc>
+@author <Lacey Perry>
+<Time.py provides 2 functions. The first, time_check, will reject any time
+already passed to prevent storage of messgaes that can't be sent. The second
+function convert_time_zone will take the users time zone and convert
+their local time to EDT.>
 """
+#note wait for Nov. 6th to make sure DST change doesn't mess this up change
+#occurs at 2:00AM
 
+import time
+from datetime import datetime, timedelta
+from src.error import CreateTweetError
 
+#dict of timezones supported by twitter keys are GMT/UTC offset
+tzsontwitter={-11:["International Date Line West", "Midway Island",
+                       "American Samoa"],
+                  -10:["Hawaii"],
+                  -8:["Alaska"],
+                  -7:["Pacific Time (US & Canada)", "Tijuana", "Arizona"],
+                  -6:["Mountain Time (US & Canada)", "Chihuahua", "Mazatlan",
+                      "Saskatchewan", "Central America"],
+                  -5:["Central Time (US & Canada", "Guadalajara", "Mexico City"
+                      ,"Monterrey", "Bogota", "Lima", "Quito"],
+                  -4:["Eastern Time (US & Canada)", "Indiana (East)", "Caracas"
+                       , "La Paz", "Georgetown"],
+                  -3:["Atlantic Time (Canada)", "Santiago", "Buenos Aires"],
+                  -2.5:["Newfoundland"],
+                  -2:["Brasilia", "Greenland", "Mid-Atlantic"],
+                  -1: ["Cape Verde Is."],
+                  0:["Azores", "Monrovia", "UTC"],
+                  1:["Dubin", "Edinburgh", "Lisbon", "London", "Casablanca",
+                     "West Central Africa"],
+                  2:["Belgrade","Bratislava", "Budapest", "Ljubljana", "Prague"
+                     , "Sarajevo", "Skopje", "Warsaw", "Zagreb", "Brussels",
+                     "Copenhagen", "Madrid", "Paris", "Amsterdam", "Berlin",
+                     "Bern", "Rome", "Stockholm", "Vienna", "Cairo", "Harare",
+                     "Pretoria"],
+                  3:["Bucharest", "Helsinki", "Kiev", "Kyiv", "Riga", "Sofia",
+                     "Talinn", "Vilnius", "Athens", "Istanbul", "Minsk",
+                     "Jerusalem", "Moscow", "St. Petersburg", "Volgograd",
+                     "Kuwait", "Riyadh", "Nairobi", "Baghdad"],
+                  3.5:["Tehran"],
+                  4:["Abu Dhabi", "Muscat", "Baku", "Tbilisi", "Yerevan"],
+                  4.5:["Kabul"],
+                  5:["Ekaterinburg", "Islamabad", "Karachi", "Tashkent"],
+                  5.5:["Koikata", "Chenmai", "Mumbai", "New Delhi"
+                       ,"Sri Jayawardenepura"],
+                  5.75:["Kathmandu"],
+                  6:["Astana", "Almaty", "Novosibirsk", "Urumqi"],
+                  6.5:["Rangoon"],
+                  7:["Bangkok", "Hanoi", "Jakarta", "Krasnoyarsk"],
+                  8:["Beijing", "Chongqing", "Hong Kong", "Kuala Lumpur",
+                     "Singapore", "Taipei", "Perth", "Irkutsk", "Ulaanbaatar"],
+                  9:["Seoul", "Osaka", "Sapporo", "Tokyo", "Yakutsk"],
+                  9.5:["Darwin"],
+                  10:["Brisbane", "Vladivsotok", "Guam", "Port Moresby",
+                      "Solomon Is."],
+                  10.5:["Adelaide"],
+                  11:["Caberra", "Melbourne", "Sydney", "Hobart", "Magadan",
+                      "New Caledonia"],
+                  12:["Fiji", "Kamchatka", "Marshall Is."],
+                  13:["Auckland", "Wellington", "Nuku'alofa"]}
+
+#EDT current time parameters
+timeutc= datetime.utcnow()
+
+#check that the requested time has not already passed. This function first
+#converts the time to EDT for ease of comparison then compares the timedelta
+#of the current time against the time requested by the user. ValueError is
+#raise if day is out of range for month or month not in range 1-12. If user
+#tries to enter an hour >12 or <0 a ValueError is thrown as well.
 def time_check(tweet_obj):
-    # insert code
-    return
+    #offset required due to DST
+    current_time=datetime(timeutc.year, timeutc.month, timeutc.day,
+                          timeutc.hour, timeutc.minute, 0)
+    current_time-=timedelta(hours=4)
 
-
-def convert_time_zone(tweet_obj):
-    #insert code
-    return
+    #convert to EDT for comparison
+    convert_time_zone(tweet_obj)
+    userhr=tweet_obj.hour
+    #conversion to 24 hr clock
+    if tweet_obj.period=="PM":
+        if userhr!=12:
+            userhr=12+userhr
     
-#retrieved from https://github.com/sixohsix/twitter/commit/694aaadfb9092a3eaa83a974f58de2cbd2cccdd8
-# Retrieved from http://docs.python.org/2/library/datetime.html on 2013-05-24
-from datetime import tzinfo, timedelta, datetime
+    """print (today)       
+    print(time.strftime("%d/%m/%Y"))
+    print("reported by utcnow=%d"%(timeutc.hour))"""        
+    userdatetime=datetime(timeutc.year, tweet_obj.month, tweet_obj.day,
+                 userhr, tweet_obj.minute, 0)
 
-ZERO = timedelta(0)
-HOUR = timedelta(hours=1)
+    now=timedelta(days=current_time.day, hours=current_time.hour,
+                  minutes=current_time.minute)
+    requested=timedelta(days=tweet_obj.day, hours=userhr,
+                        minutes=tweet_obj.minute)
 
-# A UTC class.
+    """print("Now is year %d, month %d, day %d, hour %d, minute %d\n"
+          %(timeutc.year, timeutc.month, current_time.day, current_time.hour,
+            current_time.minute))
+    print("Requested is year %d, month %d, day %d, hour %d, minute %d\n"
+          %(userdatetime.year, userdatetime.month, userdatetime.day,
+            userdatetime.hour, userdatetime.minute))"""
+  
+    #test if they requested a previous month
+    if userdate.month<current_time.month:
+        raise CreateTweetError("Time requested has passed")
 
-class UTC(tzinfo):
-    """UTC"""
-    def utcoffset(self, dt):
-        return ZERO
+    #timedelta check for days,hours,minutes
+    if requested<now:
+        raise CreateTweetError("Time requested has passed")
 
-    def tzname(self, dt):
-        return "UTC"
+    return None
 
-    def dst(self, dt):
-        return ZERO
+	
+#makes use of datetime object to store users requested time, in a 24 hr/day
+#format and calculates the difference in their time zone an EDT using a
+#timedelta object to handle difference in hours and rollover of day/month/year
+def convert_time_zone(tweet_obj):
+    edt_offset = -4
+    usertz=tweet_obj.time_zone
+    userhr=tweet_obj.hour
+    
+    #conversion to 24 hr clock
+    if tweet_obj.period=="PM":
+        if userhr!=12:
+            userhr=12+userhr
+    
+    userdatetime=datetime(timeutc.year, tweet_obj.month, tweet_obj.day, userhr,
+                 tweet_obj.minute, 0)
+    """print("year=%d, month=%d, day=%d, hour=%d, minute=%d"%(userdatetime.year
+          , userdatetime.month, userdatetime.day, userdatetime.hour,
+            userdatetime.minute))"""
 
-utc = UTC()
+    #check for user time zone and convert as needed.
+    if usertz=="Eastern Time (US & Canada)" or usertz=="Indiana (East)":
+        return None
+    else:
+        for keys,vals in tzsontwitter.items():
+            for x in vals:
+                if x==usertz:
+                    tempmin=timedelta(minutes=0)
+                    #check for 1/2 and 3/4 timezones
+                    if keys%1==.5:
+                        tempmin=timedelta(minutes=30)
+                        
+                    if keys%1==.75:
+                        tempmin=timedelta(minutes=45)
+                        
+                    userdatetime+tempmin
+                    temphr=timedelta(hours=keys-edt_offset)
 
-# A class building tzinfo objects for fixed-offset time zones.
-# Note that FixedOffset(0, "UTC") is a different way to build a
-# UTC tzinfo object.
+                    userdatetime-=temphr
 
-class FixedOffset(tzinfo):
-    """Fixed offset in minutes east from UTC."""
-
-    def __init__(self, offset, name):
-        self.__offset = timedelta(minutes = offset)
-        self.__name = name
-
-    def utcoffset(self, dt):
-        return self.__offset
-
-    def tzname(self, dt):
-        return self.__name
-
-    def dst(self, dt):
-        return ZERO
-
-# A class capturing the platform's idea of local time.
-
-import time as _time
-
-STDOFFSET = timedelta(seconds = -_time.timezone)
-if _time.daylight:
-    DSTOFFSET = timedelta(seconds = -_time.altzone)
-else:
-    DSTOFFSET = STDOFFSET
-
-DSTDIFF = DSTOFFSET - STDOFFSET
-
-class LocalTimezone(tzinfo):
-
-    def utcoffset(self, dt):
-        if self._isdst(dt):
-            return DSTOFFSET
+                    """print("year=%d, month=%d, day=%d, hour=%d, minute=%d"%(userdatetime.year,
+                            userdatetime.month, userdatetime.day, userdatetime.hour,
+                            userdatetime.minute))"""
+                    break
+                
+        #convert time back to 12 hr format        
+        convert= userdatetime.hour
+        if userdatetime.hour>=12:
+            convert=userdatetime.hour-12
+            tweet_obj.period="PM"
         else:
-            return STDOFFSET
+            tweet_obj.period="AM"
 
-    def dst(self, dt):
-        if self._isdst(dt):
-            return DSTDIFF
-        else:
-            return ZERO
+        #convert the tweet_obj fields    
+        tweet_obj.month=userdatetime.month
+        tweet_obj.day=userdatetime.day
+        tweet_obj.hour=convert
+        tweet_obj.minute=userdatetime.minute        
 
-    def tzname(self, dt):
-        return _time.tzname[self._isdst(dt)]
-
-    def _isdst(self, dt):
-        tt = (dt.year, dt.month, dt.day,
-              dt.hour, dt.minute, dt.second,
-              dt.weekday(), 0, 0)
-        stamp = _time.mktime(tt)
-        tt = _time.localtime(stamp)
-        return tt.tm_isdst > 0
-
-Local = LocalTimezone()
+    return None			
