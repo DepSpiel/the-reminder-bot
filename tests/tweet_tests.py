54115621d3afe1ffa5c763bbf7bc50d1b70b03fc
Brendan Babcock <babcock.brendan@gmail.com>
2016-11-28 00:52:05 +0000

    tweet_tests, with a few fixes implemented.

--- tests/tweet_tests.py
+++ tests/tweet_tests.py
@@ -15,108 +15,108 @@ import json
 
 
 class TweetTest(unittest.TestCase):
-    # Each function indicates a test case for the filter.py file. This file and the tests within it are based on time_tests_2, with a few additions (testing 'null', etc.)
+    # Each function indicates a test case for the tweet.py file. 
 
     def test_tweet1(self):
-        test_obj = json.dumps('8:45 : AM : 12/26 : Wake up!')
+        test_obj = json.loads('{"direct_message" : {"text" : "8:45 : AM : 12/26 : Wake up!", "sender_screen_name" : "myTwitterHandle", "sender" : {"time_zone" : "Atlantic Time (Canada)"}, "recipient" : { "following" : "true"}}}')
         tested_Tweet = create_tweet(test_obj)
-        self.assertEqual(tested_Tweet.hour, 12)
-        self.assertEqual(tested_Tweet.minute, 45)
+        self.assertEqual(tested_Tweet.hour, "8")
+        self.assertEqual(tested_Tweet.minute, "45")
         self.assertEqual(tested_Tweet.period, "AM")
-        self.assertEqual(tested_Tweet.month, "8")
+        self.assertEqual(tested_Tweet.month, "12")
         self.assertEqual(tested_Tweet.day, "26")
-        self.assertEqual(tested_Tweet.msg, "Wake up!")
+        self.assertEqual(tested_Tweet.msg, " Wake up!")
 
 
     def test_tweet2(self):
-        test_obj = json.dumps('8:45 : AM : 12/26 : Wake up! : Extra Field')
+        test_obj = json.loads('{"direct_message" : {"text" : "8:45 : AM : 12/26 : Wake up!", "sender_screen_name" : "myTwitterHandle", "sender" : {"time_zone" : "Atlantic Time (Canada)"}, "recipient" : { "following" : "false"}}}')
         tested_Tweet = create_tweet(test_obj)
-        self-assertRaises(CreateTweetError)
+        self.assertRaises(CreateTweetError)
 
     def test_tweet3(self):
-        test_obj = json.dumps('8:45 : AM : 12/26 : Wake up! : Extra Field : Extra Extra Field')
+        test_obj = json.loads('{"direct_message" : {"text" : "8:45 : AM : 15/26 : Wake up!", "sender_screen_name" : "myTwitterHandle", "sender" : {"time_zone" : "Atlantic Time (Canada)"}, "recipient" : { "following" : "true"}}}')
         tested_Tweet = create_tweet(test_obj)
-        self-assertRaises(CreateTweetError)
+        self.assertRaises(CreateTweetError)
 
     def test_tweet4(self):
-        test_obj = json.dumps('8:45  AM  12/26  Wake up!  Extra Field')
+        test_obj = json.loads('{"direct_message" : {"text" : "8:45 : AM : 12/26 : Wake up!", "sender_screen_name" : "myTwitterHandle", "sender" : {"time_zone" : ""}, "recipient" : { "following" : "true"}}}')
         tested_Tweet = create_tweet(test_obj)
-        self-assertRaises(CreateTweetError)
+        self.assertRaises(CreateTweetError)
 
     def test_tweet4(self):
-        test_obj = json.dumps('8:45 / AM / 12/26 / Wake up! / Extra Field')
+        test_obj = json.loads('{"direct_message" : {"text" : "8:45 : AM : 9/26 : Wake up!", "sender_screen_name" : "myTwitterHandle", "sender" : {"time_zone" : "Atlantic Time (Canada)"}, "recipient" : { "following" : "true"}}}')
         tested_Tweet = create_tweet(test_obj)
-        self-assertRaises(CreateTweetError)
+        self.assertRaises(CreateTweetError)
 
     def test_tweet5(self):
-        test_obj = json.dumps('8:45 ::: AM : 12/26 : Wake up! : Extra Field')
+        test_obj = json.loads('{"direct_message" : {"text" : "14:45 : AM : 12/26 : Wake up!", "sender_screen_name" : "myTwitterHandle", "sender" : {"time_zone" : "Atlantic Time (Canada)"}, "recipient" : { "following" : "true"}}}')
         tested_Tweet = create_tweet(test_obj)
-        self-assertRaises(CreateTweetError)
+        self.assertRaises(CreateTweetError)
 
     def test_tweet6(self):
-        test_obj = json.dumps('::::')
+        test_obj = json.loads('{"direct_message" : {"text" : "8:45 : AM : 12/54 : Wake up!", "sender_screen_name" : "myTwitterHandle", "sender" : {"time_zone" : "Atlantic Time (Canada)"}, "recipient" : { "following" : "true"}}}')
         tested_Tweet = create_tweet(test_obj)
-        self-assertRaises(CreateTweetError)
+        self.assertRaises(CreateTweetError)
 
     def test_tweet7(self):
-        test_obj = json.dumps('8:45 ::: ')
+        test_obj = json.loads('{"direct_message" : {"text" : "8:45 :  : 12/26 : Wake up!", "sender_screen_name" : "myTwitterHandle", "sender" : {"time_zone" : "Atlantic Time (Canada)"}, "recipient" : { "following" : "true"}}}')
         tested_Tweet = create_tweet(test_obj)
-        self-assertRaises(CreateTweetError)
+        self.assertRaises(CreateTweetError)
 
     def test_tweet8(self):
-        test_obj = json.dumps('845 :: AM : 12/26 : Wake up!')
+        test_obj = json.loads('{"direct_message" : {"text" : "845 : AM : 12/26 : Wake up!", "sender_screen_name" : "myTwitterHandle", "sender" : {"time_zone" : "Atlantic Time (Canada)"}, "recipient" : { "following" : "true"}}}')
         tested_Tweet = create_tweet(test_obj)
-        self-assertRaises(CreateTweetError)
+        self.assertRaises(CreateTweetError)
 
     def test_tweet8(self):
-        test_obj = json.dumps('8:45AM12/26Wake up!Extra Field:::')
+        test_obj = json.loads('{"direct_message" : {"text" : "8:45 : AM :  : Wake up!", "sender_screen_name" : "myTwitterHandle", "sender" : {"time_zone" : "Atlantic Time (Canada)"}, "recipient" : { "following" : "true"}}}')
         tested_Tweet = create_tweet(test_obj)
-        self-assertRaises(CreateTweetError)
+        self.assertRaises(CreateTweetError)
 
     def test_tweet9(self):
-        test_obj = json.dumps('8:45AM12::/26Wake up!')
+        test_obj = json.loads('{"direct_message" : {"text" : "!@:%^ : AM : 12/26 : Wake up!", "sender_screen_name" : "myTwitterHandle", "sender" : {"time_zone" : "Atlantic Time (Canada)"}, "recipient" : { "following" : "true"}}}')
         tested_Tweet = create_tweet(test_obj)
-        self-assertRaises(CreateTweetError)
+        self.assertRaises(CreateTweetError)
 
     def test_tweet10(self):
-        test_obj = json.dumps('8:45 : AM : 12/26&& : Wake up!')
+        test_obj = json.loads('{"direct_message" : {"text" : "!@:#$ : () : 12/26 : Wake up!", "sender_screen_name" : "myTwitterHandle", "sender" : {"time_zone" : "Atlantic Time (Canada)"}, "recipient" : { "following" : "true"}}}')
         tested_Tweet = create_tweet(test_obj)
-        self-assertRaises(CreateTweetError)
+        self.assertRaises(CreateTweetError)
 
     def test_tweet11(self):
-        test_obj = json.dumps('8:45 : AM : 12!@#$%^/2 : Wake up!')
+        test_obj = json.loads('{"direct_message" : {"text" : " :  :  : !", "sender_screen_name" : "myTwitterHandle", "sender" : {"time_zone" : "Atlantic Time (Canada)"}, "recipient" : { "following" : "true"}}}')
         tested_Tweet = create_tweet(test_obj)
-        self-assertRaises(CreateTweetError)
+        self.assertRaises(CreateTweetError)
 
     def test_tweet12(self):
-        test_obj = json.dumps('34:45 : AM : 12/26 : Wake up!')
+        test_obj = json.loads('{"direct_message" : {"text" : "-8:45 : AM : 12/26 : Wake up!", "sender_screen_name" : "myTwitterHandle", "sender" : {"time_zone" : "Atlantic Time (Canada)"}, "recipient" : { "following" : "true"}}}')
         tested_Tweet = create_tweet(test_obj)
-        self-assertRaises(CreateTweetError)
+        self.assertRaises(CreateTweetError)
 
     def test_tweet13(self):
-        test_obj = json.dumps('34:67 : AM : 12/26 : Wake up!')
+        test_obj = json.loads('{"direct_message" : {"text" : "8:-45 : AM : 12/26 : Wake up!", "sender_screen_name" : "myTwitterHandle", "sender" : {"time_zone" : "Atlantic Time (Canada)"}, "recipient" : { "following" : "true"}}}')
         tested_Tweet = create_tweet(test_obj)
-        self-assertRaises(CreateTweetError)
+        self.assertRaises(CreateTweetError)
 
     def test_tweet14(self):
-        test_obj = json.dumps('0005:45 : AM : 12/26 : Wake up!')
+        test_obj = json.loads('{"direct_message" : {"text" : " :  :  : !!!", "sender_screen_name" : "myTwitterHandle", "sender" : {"time_zone" : ""}, "recipient" : { "following" : "true"}}}')
         tested_Tweet = create_tweet(test_obj)
-        self-assertRaises(CreateTweetError)
+        self.assertRaises(CreateTweetError)
 
     def test_tweet15(self):
-        test_obj = json.dumps('10:45_:_AM_:_12/26_:_Wake up!')
+        test_obj = json.loads('{"direct_message" : {"text" : "8:45 : AM : 12/26 : Wake up!", "sender_screen_name" : "myTwitterHandle", "sender" : {"time_zone" : "Atlantic Time (Canada)"}, "recipient" : { "following" : "!@#$%^&*("}}}')
         tested_Tweet = create_tweet(test_obj)
-        self-assertRaises(CreateTweetError)
+        self.assertRaises(CreateTweetError)
 
     def test_tweet16(self):
-        test_obj = json.dumps('10:45 : AM : 12 26 : Wake up!')
+        test_obj = json.loads('{"direct_message" : {"text" : "8::45 : AM : 12/26 : Wake up!", "sender_screen_name" : "myTwitterHandle", "sender" : {"time_zone" : "Atlantic Time (Canada)"}, "recipient" : { "following" : "true"}}}')
         tested_Tweet = create_tweet(test_obj)
-        self-assertRaises(CreateTweetError)
+        self.assertRaises(CreateTweetError)
 
     def test_tweet17(self):
-        test_obj = json.dumps('10:45 : AM : 12   /   26 : Wake up!')
+        test_obj = json.loads('{"direct_message" : {"text" : "8_  :  45_ : AM_ : 12  /  _26 : Wake up!", "sender_screen_name" : "myTwitterHandle", "sender" : {"time_zone" : "Atlantic Time (Canada)"}, "recipient" : { "following" : "true"}}}')
         tested_Tweet = create_tweet(test_obj)
-        self-assertRaises(CreateTweetError)
+        self.assertRaises(CreateTweetError)
   
 
 
